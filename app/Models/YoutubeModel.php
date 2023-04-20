34078b45d13edc7182efbc7fe2745156ffff7944
bondanjody <bondanjodys@gmail.com>
2023-04-20 07:48:14 +0700

    Menambahkan Model

---
+++ app/Models/YoutubeModel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class YoutubeModel extends Model
+{
+    protected $table = 'youtube-index';
+
+    protected $useTimestamps = true;
+
+    protected $allowedFields = ['judul', 'nama_channel', 'category', 'slug', 'gambar'];
+
+    public function getData($slug = false)
+    {
+        if ($slug == false) 
+        {
+            return $this->findAll();
+        }
+
+        return $this->where(['slug' => $slug])->first();
+    }
+}
+
+?>
